80e43d8b4a4875cb384490909ba06e86bb7e72bf
CB721 <clintbrodar@gmail.com>
2019-07-30 07:51:24 -0700

    Tribonacci test cases

--- py/jul29.py
+++ py/jul29.py
@@ -1,6 +1,7 @@
 # summing the last 3 (instead of 2) numbers of the sequence to generate the next
 # Signature will always contain 3 numbers 
 # n will always be a non-negative number
+# n is number of tribonacci numbers
 def tribonacci(signature, n):
     # store numbers
     numArr = []
@@ -14,3 +15,11 @@ def tribonacci(signature, n):
         numArr.append(signature.pop(0))
 
     return numArr
+
+# test cases
+print(tribonacci([1, 1, 1], 10))
+# [1, 1, 1, 3, 5, 8, 17, 31, 57, 105]
+print(tribonacci([3, 2, 4], 5))
+# [3, 2, 4, 9, 15]
+print(tribonacci([-1, 4, -4], 15))
+# [-1, 4, -4, -1, -1, -6, -8, -15, -29, -52, -96, -177, -325, -598, -1100]
