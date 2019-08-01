5ebdd59ede3e68f5a3320f1ca3813002afdc2bbe
CB721 <clintbrodar@gmail.com>
2019-08-01 15:07:23 -0700

    Triangle exercise

---
+++ js/aug01.js
@@ -0,0 +1,27 @@
+// Implement a method that accepts 3 integer values a, b, c. 
+// The method should return true if a triangle can be built with the sides of given length and false in any other case.
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+function isTriangle(a, b, c) {
+    // all sides must be greater than zero
+    if (a <= 0 || b <= 0 || c <= 0) {
+        return false;
+    }
+    // values of any two must be greater than the third
+    if ((a + b > c) && (c + b > a) && (a + c > b)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+// test cases
+console.log(isTriangle(1, 2, 2));
+// true
+console.log(isTriangle(7, 2, 2));
+// false
+console.log(isTriangle(-1, 2, 2));
+// false
+console.log(isTriangle(0, 2, 2));
+// false
+console.log(isTriangle(31, 26, 6));
+// true
