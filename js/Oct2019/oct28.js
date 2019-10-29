2fd440a2739622cd8131d445313fea245463bf99
Clint Brodar <clintbrodar@gmail.com>
2019-10-29 09:16:51 -0700

    Selection sort

--- js/Oct2019/oct28.js
+++ js/Oct2019/oct28.js
@@ -8,7 +8,7 @@ expandedForm = (num) => {
         });
     let numStr = [];
     for (let i = 0; i < splitNum.length; i++) {
-        if(splitNum[i] !== "0") {
+        if (splitNum[i] !== "0") {
             let newNum = splitNum[i];
             for (let j = 0; j < splitNum.length - i - 1; j++) {
                 newNum += "0";
@@ -60,3 +60,26 @@ console.log(insertionSort([45, 300, 2, 1400, 42, 500, 30000, 32]));
 console.log(insertionSort([900, 3, 73, 75, 7, 89]));
 // 3, 7, 73, 75, 89, 900
 
+// selection sort
+selectionSort = (arr) => {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let smallestIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[smallestIndex] > arr[j]) {
+                smallestIndex = j;
+            }
+        }
+        let temp = arr[i];
+        arr[i] = arr[smallestIndex];
+        arr[smallestIndex] = temp;
+    }
+    return arr;
+}
+
+// test cases
+console.log(selectionSort([16, 5, 11, 8, 1, 2, 20]));
+// 1, 2, 5, 8, 11, 16, 20
+console.log(selectionSort([6, -15, 1, 8, 0, 2, -15]));
+// -15, -15, 0, 1, 2, 6, 8
+console.log(selectionSort([600, -1, 1, 8, 0, 2, 600]));
+// -1, 0, 1, 2, 8, 600, 600
