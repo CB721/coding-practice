1de9388f896513d30f12f083a90ddec50571aeaf
Clint Brodar <clintbrodar@gmail.com>
2019-12-28 12:28:51 -0800

    Palindrome every solution

--- js/Algorithms/palindromes.js
+++ js/Algorithms/palindromes.js
@@ -1,11 +1,19 @@
 // given a string, return true if the string is a palindrome or false if it is not
 
+// function palindrome(str) {
+//     if (str === str.split('').reverse().join('')) {
+//         return true
+//     } else {
+//         return false;
+//     }
+// }
+// function palindrome(str) {
+//     return str === str.split('').reverse().join('');
+// }
 function palindrome(str) {
-    if (str === str.split('').reverse().join('')) {
-        return true
-    } else {
-        return false;
-    }
+    return str.split('').every(function(letter, index) {
+        return letter === str[str.length - index - 1]
+    })
 }
 
 console.log(palindrome("mom"));
