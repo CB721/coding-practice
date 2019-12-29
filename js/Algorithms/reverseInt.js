1d8f14485f9de8434bc6a389e0929a60bb558d60
Clint Brodar <clintbrodar@gmail.com>
2019-12-28 18:23:29 -0800

    Reverse int second solution

--- js/Algorithms/reverseInt.js
+++ js/Algorithms/reverseInt.js
@@ -1,10 +1,22 @@
 // given an integer, return an integer that is the reverse ordering of numbers
 
+// function reverseInt(int) {
+//     const isPositive = Math.sign(int);
+//     const intStr = int.toString().split('').reverse().join('');
+//     const newInt = parseInt(intStr);
+//     return isPositive * newInt;
+// }
 function reverseInt(int) {
-    const isPositive = Math.sign(int);
-    const intStr = int.toString().split('').reverse().join('');
-    const newInt = parseInt(intStr);
-    return isPositive * newInt;
+    const intArr = int.toString().split('').reverse();
+    let intStr = '';
+    for (let i = 0; i < intArr.length; i++) {
+        if (intArr[i] === '-') {
+            intStr = intArr[i] + intStr;
+        } else {
+            intStr = intStr + intArr[i];
+        }
+    }
+    return parseInt(intStr);
 }
 
 console.log(reverseInt(15));
