0f8ffb3d03670459a78885637cc518e02d576ba4
Clint Brodar <clintbrodar@gmail.com>
2019-12-30 07:48:08 -0800

    Array chunk slice solution

--- js/Algorithms/arrChunk.js
+++ js/Algorithms/arrChunk.js
@@ -1,14 +1,23 @@
 // given an array and chunk size, dived the array into many subarrays where each subarray is of length size
 
+// function chunk(arr, size) {
+//     const outArr = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         const last = outArr[outArr.length - 1];
+//         if (!last || last.length === size) {
+//             outArr.push([arr[i]]);
+//         } else {
+//             last.push(arr[i]);
+//         }
+//     }
+//     return outArr;
+// }
 function chunk(arr, size) {
     const outArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        const last = outArr[outArr.length - 1];
-        if (!last || last.length === size) {
-            outArr.push([arr[i]]);
-        } else {
-            last.push(arr[i]);
-        }
+    for (let i = 0; i < arr.length; i += size) {
+        let sizeSlice = size + i;
+        let tempArr = arr.slice(i, sizeSlice);
+        outArr.push(tempArr);
     }
     return outArr;
 }
