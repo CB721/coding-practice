d9323e4f943641d7358fc385e483707aa87f68d4
Clint Brodar <clintbrodar@gmail.com>
2019-12-30 10:09:37 -0800

    Array prefill

---
+++ js/Dec2019/dec30.js
@@ -0,0 +1,32 @@
+// Create the function prefill that returns an array of n elements that all have the same value v. See if you can do this without using a loop.
+
+// You have to validate input:
+
+// v can be anything (primitive or otherwise)
+// if v is ommited, fill the array with undefined
+// if n is 0, return an empty array
+// if n is anything other than an integer or integer-formatted string (e.g. '123') that is >=0, throw a TypeError
+// n cannot be infinity, negative infinity, not a number, a decimal number or a boolean
+// When throwing a TypeError, the message should be n is invalid, where you replace n for the actual value passed to the function.
+
+function prefill(n, v) {
+    let fillNum = parseInt(n);
+    if (n === Infinity || n === -Infinity || n === NaN || n % 1 !== 0 || n < 0 || typeof(n) === "boolean") {
+        throw TypeError (n + " is invalid");
+    } else if (Number.isInteger(fillNum)) {
+        return Array.from(Array(+n), x => v);
+    } else if (v.length < 1) {
+        return [undefined]
+    } else if (n === 0) {
+        return [];
+    }
+}
+
+console.log(prefill(3, 1));
+// [1, 1, 1]
+console.log(prefill(2, "abc"));
+// ["abc", "abc"]
+console.log(prefill("3", prefill(2, '2d')));
+// [[2d, 2d], [2d, 2d], [2d, 2d]]
+console.log(prefill("xyz", 1));
+// throws TypeError with message "xyz is invalid"
