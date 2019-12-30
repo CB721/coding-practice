3171db108d6dcbae3ba7fabeb91acfb587a33e75
Clint Brodar <clintbrodar@gmail.com>
2019-12-30 08:51:52 -0800

    Anagrams object key map solution

--- js/Algorithms/anagrams.js
+++ js/Algorithms/anagrams.js
@@ -3,10 +3,31 @@
 // only consider characters, not spaces or punctuation
 // capital letters are considered the same as lowercase
 
+// function anagrams(strA, strB) {
+//     const lowerA = strA.toLowerCase().split('').sort().join('').replace(/[^a-zA-Z\d\s:]/g, '');
+//     const lowerB = strB.toLowerCase().split('').sort().join('').replace(/[^a-zA-Z\d\s:]/g, '');
+//     return lowerA === lowerB;
+// }
 function anagrams(strA, strB) {
-    const lowerA = strA.toLowerCase().split('').sort().join('').replace(/[^a-zA-Z\d\s:]/g, '');
-    const lowerB = strB.toLowerCase().split('').sort().join('').replace(/[^a-zA-Z\d\s:]/g, '');
-    return lowerA === lowerB;
+    const aCharMap = buildCharMap(strA);
+    const bCharMap = buildCharMap(strB);
+    if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+        return false;
+    } else {
+        for (let char in aCharMap) {
+            if (aCharMap[char] !== bCharMap[char]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+function buildCharMap(str) {
+    const charMap = {};
+    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+    return charMap;
 }
 
 console.log(anagrams('rail safety', 'fairy tales'));
