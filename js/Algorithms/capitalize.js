41943a3183e5a2fb68434ad56d19c93b83903c72
Clint Brodar <clintbrodar@gmail.com>
2019-12-30 09:14:42 -0800

    Letter capitalization slice solution

--- js/Algorithms/capitalize.js
+++ js/Algorithms/capitalize.js
@@ -1,16 +1,25 @@
 // write a function that accepts a string
 // it should capitalize the first letter of each word in the string
 
+// function capitalize(str) {
+//     const splitStr = str.split(' ');
+//     for (let i = 0; i < splitStr.length; i++) {
+//         let firstLetter = splitStr[i][0].toUpperCase();
+//         for (let j = 1; j < splitStr[i].length; j++) {
+//             firstLetter += splitStr[i][j];
+//         }
+//         splitStr.splice(i, 1, firstLetter);
+//     }
+//     return splitStr.join(' ');
+// }
 function capitalize(str) {
     const splitStr = str.split(' ');
+    const newStr = [];
     for (let i = 0; i < splitStr.length; i++) {
-        let firstLetter = splitStr[i][0].toUpperCase();
-        for (let j = 1; j < splitStr[i].length; j++) {
-            firstLetter += splitStr[i][j];
-        }
-        splitStr.splice(i, 1, firstLetter);
+        let word = splitStr[i][0].toUpperCase() + splitStr[i].slice(1);
+        newStr.push(word);
     }
-    return splitStr.join(' ');
+    return newStr.join(' ');
 }
 
 console.log(capitalize('a short sentence'));
