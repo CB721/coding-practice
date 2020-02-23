4a546c63691941632cae3a7b245f218655d58dc9
Clint Brodar <clintbrodar@gmail.com>
2020-02-23 10:00:34 -0800

    Memoize fibonacci

--- js/Algorithms/fibonacci.js
+++ js/Algorithms/fibonacci.js
@@ -1,6 +1,8 @@
 // the fibonacci series is an ordering of numbers where each number is the sum of the proceeding two
 // print out the n-th entry in the fibonacci series
 
+// linear runtime solution
+// O(n)
 // function fib(n) {
 //     // check for positive number
 //     if (n < 0) {
@@ -26,12 +28,55 @@
 // console.log(fib(-4));
 // // "n must be a positive number"
 
-function fib(n) {
+// exponential runtime solution
+// 2^n, increasing by one, ~ doubles the amount of time to complete
+// function fib(n) {
+//     if (n < 2) {
+//         return n;
+//     }
+//     return (fib(n -1) + fib(n -2));
+// }
+
+// console.log(fib(4));
+// // 3
+// console.log(fib(9));
+// // 34
+// console.log(fib(0));
+// // 0
+
+// memoization - store the arguments of each function call along with the result
+// if the function is called again with the same arguments, return the precomputed result rather than run the function again
+
+// pass in a function and return a faster version of said function
+function memoize(fn) {
+    // declare storage area
+    // store previous arguments and results
+    const cache = {};
+    // unknown how many arguments will be passed into function
+    return function(...args) {
+        // check if this function has been called with this set of arguments before
+        if (cache[args]) {
+            return cache[args];
+        // if not, that means the function has not been called with this set of arguments before
+        } else {
+            // call original function aka 'slowFib' and save result to a variable
+            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/apply
+            const result = fn.apply(this, args);
+            // create key value pair of result to cache object
+            cache[args] = result;
+            return result;
+        }
+    }
+}
+
+function slowFib(n) {
     if (n < 2) {
         return n;
     }
-    return (fib(n -1) + fib(n -2));
+    return (slowFib(n -1) + slowFib(n -2));
 }
+
+const fib = memoize(slowFib)
 
 console.log(fib(4));
 // 3
