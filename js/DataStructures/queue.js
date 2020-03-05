b7ac5b259f2eda2073cc3f5a645aa76d0c44241f
Clint Brodar <clintbrodar@gmail.com>
2020-03-05 10:41:25 -0800

    Queue

---
+++ js/DataStructures/queue.js
@@ -0,0 +1,22 @@
+// create a queue data structure
+// it should be a class with methods 'add' and 'remove'
+// adding to the que should store an element until it is removed
+
+// create a class to use as a template
+class Queue {
+    constructor() {
+        // use an array to hold the data
+        this.data = [];
+    }
+    // method to add to queue
+    add(record) {
+        // add to the beginning of the data array
+        this.data.unshift(record);
+    }
+    // method to remove from queue
+    remove(record) {
+        // remove from the end of the data array
+        // return so that we can do something with the removed record
+        return this.data.pop(record);
+    }
+}
