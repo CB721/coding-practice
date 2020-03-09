758afcef1045d602684eca7676d1752d644cbddb
Clint Brodar <clintbrodar@gmail.com>
2020-03-08 19:18:05 -0700

    Hello C#

---
+++ c#/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Please enter your name");
+            name = Console.ReadLine();
+            Console.WriteLine("Please enter your age");
+            age = Convert.ToInt32(Console.ReadLine());
+            if (age > 18)
+            {
+                Console.WriteLine(name + ", you are free to enter");
+            } else
+            {
+                Console.WriteLine("Sorry, " + name + ". You are not allowed in");
+            }
+        }
+    }
+}
