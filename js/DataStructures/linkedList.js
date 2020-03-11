1b206354cb2b808643b23b8441dda4ba03466121
Clint Brodar <clintbrodar@gmail.com>
2020-03-10 20:10:08 -0700

    Node and linkedlist initial classes

---
+++ js/DataStructures/linkedList.js
@@ -0,0 +1,27 @@
+// create a class instance to represent a node
+    // it should have two properites: "data" and "next"
+    // accept both as arguments to the "node" constructor and assign them to the instance as properties
+    // if next is not provided, its default value should be null
+// create a class instance to represent a linked list
+    // it should not have a head node associated with it
+    // it will have one property "head" which is a reference to the first node in the list
+    // by default, the head should be null
+// create a new node from argument "data"
+    // assign the resulting node to the head property
+    // make sure to handle the case in which the linked list already has a node assigned to the head property
+ 
+// implement classes Node and Linked Lists
+
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+    
+}
