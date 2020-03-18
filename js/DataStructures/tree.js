3b1c76a3d458da8d2d791453f36f87ed1e9f049d
Clint Brodar <clintbrodar@gmail.com>
2020-03-17 20:43:11 -0700

    Tree node

---
+++ js/DataStructures/tree.js
@@ -0,0 +1,25 @@
+// create a node class
+// the node constructor should accept an argument that gets assigned to the data property and initialize an empty array for storing children
+// the node class should have methods 'add' and 'remove'
+// create a tree class
+// the tree constructor should initialize a 'root' property to null
+// implement 'traverseBFS' and 'traverseDFS' on the tree class
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.children = [];
+    }
+    add(data) {
+        this.children.push(new Node(data));
+    }
+    remove(data) {
+        this.children = this.children.filter(child => child.data !== data);
+    }
+}
+
+class Tree {
+
+}
+
+module.exports = { Tree, Node };
