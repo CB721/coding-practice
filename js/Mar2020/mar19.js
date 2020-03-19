91db00e99c5ed2f4ba642bf53cf80ee53ae07eed
Clint Brodar <clintbrodar@gmail.com>
2020-03-19 08:36:44 -0700

    Array difference

--- js/Mar2020/mar19.js
+++ js/Mar2020/mar19.js
@@ -16,13 +16,34 @@ function list(names) {
     return outStr;
 }
 
-console.log(list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'},{name: 'Homer'},{name: 'Marge'}]));
-// 'Bart, Lisa, Maggie, Homer & Marge'
-console.log(list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'}]));
-// 'Bart, Lisa & Maggie'
-console.log(list([{name: 'Bart'},{name: 'Lisa'}]));
-// 'Bart & Lisa'
-console.log(list([{name: 'Bart'}]));
-// 'Bart'
-console.log(list([]));
-// ''
+// console.log(list([{ name: 'Bart' }, { name: 'Lisa' }, { name: 'Maggie' }, { name: 'Homer' }, { name: 'Marge' }]));
+// // 'Bart, Lisa, Maggie, Homer & Marge'
+// console.log(list([{ name: 'Bart' }, { name: 'Lisa' }, { name: 'Maggie' }]));
+// // 'Bart, Lisa & Maggie'
+// console.log(list([{ name: 'Bart' }, { name: 'Lisa' }]));
+// // 'Bart & Lisa'
+// console.log(list([{ name: 'Bart' }]));
+// // 'Bart'
+// console.log(list([]));
+// // ''
+
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b.
+
+function arrayDiff(a, b) {
+    const outArr = [];
+    for (let i = 0; i < a.length; i++) {
+        if (!b.includes(a[i])) {
+            outArr.push(a[i]);
+        }
+    }
+    return outArr;
+}
+
+console.log(arrayDiff([], [4, 5]));
+// []
+console.log(arrayDiff([3, 4], [3]));
+// [4]
+console.log(arrayDiff([1, 8, 2], []));
+// [1, 8, 2]
