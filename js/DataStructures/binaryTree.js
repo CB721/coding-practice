639b4e8a0ecb429b350a9e1ace9f9289458ed7a4
Clint Brodar <clintbrodar@gmail.com>
2020-03-19 20:59:26 -0700

    Binary search tree contains

--- js/DataStructures/binaryTree.js
+++ js/DataStructures/binaryTree.js
@@ -2,6 +2,9 @@
 // the constructo should initialize values 'data', 'left' and 'right'
 // implement the 'insert' method for the node class
 // the insert should accept an argument 'data' and then insert a new node at the appropriate location in the tree
+// implement the 'contains' method for the node class
+// contains should accept a 'data' argument and return the node in the tree with the same value
+// if the value isn't in the tree, return null
 
 class Node {
     constructor(data) {
@@ -20,6 +23,19 @@ class Node {
             this.right = new Node(data);
         }
     }
+    contains(data) {
+        if (data === this.data) {
+            return this;
+        } else if (data < this.data && this.left) {
+            return this.left.contains(data);
+        } else if (data < this.data) {
+            return null;
+        } else if (data > this.data && this.right) {
+            return this.right.contains(data);
+        } else if (data > this.data) {
+            return null
+        }
+    }
 }
 
 const node = new Node("n");
@@ -28,4 +44,13 @@ node.insert("c");
 node.insert("l");
 node.insert("t");
 
-console.log(node);
+// console.log(node);
+console.log(node.contains("c"))
+// Node { data: 'c', right: null, left: null }
+console.log(node.contains("i"));
+// Node {
+//     data: 'i',
+//     right: Node { data: 'l', right: null, left: null },
+//     left: Node { data: 'c', right: null, left: null } }
+console.log(node.contains("z"));
+// null
