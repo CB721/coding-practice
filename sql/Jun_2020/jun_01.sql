c670848d5762388508261178419aaae1003b2873
Clint Brodar <clintbrodar@gmail.com>
2020-06-01 11:58:29 -0700

    Additional aggregate exercises

---
+++ sql/Jun_2020/jun_01.sql
@@ -0,0 +1,38 @@
+-- Write a query that displays only the state with the largest amount of fruit supply.
+SELECT state, COUNT(*) 
+FROM fruit_imports
+GROUP BY state
+ORDER BY COUNT(*) DESC
+LIMIT 1;
+
+-- Write a query that returns the most expensive cost_per_unit of every season. The query should display 2 columns, the season and the cost_per_unit
+SELECT season, MAX(cost_per_unit) 
+FROM fruit_imports
+GROUP BY season;
+
+-- Write a query that returns the state that has more than 1 import of the same fruit. 
+SELECT state, name, COUNT(*) 
+FROM fruit_imports
+GROUP BY state, name
+HAVING COUNT(*) > 1;
+
+-- Write a query that returns the seasons that produce either 3 fruits or 4 fruits.
+SELECT season
+FROM fruit_imports
+GROUP BY season
+HAVING COUNT(name) = 3 OR COUNT(name) = 4;
+
+-- Write a query that takes into consideration the  supply and cost_per_unit columns for determining the total cost and returns the most expensive state with the total cost.
+SELECT supply * cost_per_unit AS total_cost
+FROM fruit_imports
+ORDER BY supply * cost_per_unit DESC
+LIMIT 1;
+
+-- Execute the below SQL script and answer the question that follows:
+-- CREATE table fruits (fruit_name varchar(10));
+-- INSERT INTO fruits VALUES ('Orange');
+-- INSERT INTO fruits VALUES ('Apple');
+-- INSERT INTO fruits VALUES (NULL);
+-- INSERT INTO fruits VALUES (NULL);
+-- Write a query that returns the count of 4. You'll need to count on the column fruit_name and not use COUNT(*)
+SELECT COUNT(1) FROM fruits;
