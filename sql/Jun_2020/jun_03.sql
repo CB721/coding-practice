258f5665f181bf8b640771f7809a1239a9e2355e
Clint Brodar <clintbrodar@gmail.com>
2020-06-03 13:27:20 -0700

    Joins

--- sql/Jun_2020/jun_03.sql
+++ sql/Jun_2020/jun_03.sql
@@ -10,4 +10,33 @@ FROM employees, regions
 WHERE employees.region_id = regions.region_id
 GROUP BY regions.country;
 
+-- return the total number of employees that work in each country using a join
 
+SELECT COUNT(employee_id) AS total_employees, country 
+FROM employees INNER JOIN regions
+ON employees.region_id = regions.region_id
+GROUP BY regions.country;
+
+-- return the first name, email, country and division for each employee.  replace any null emails with 'N/A' using a join
+SELECT first_name, COALESCE(email, 'N/A') AS email, division, country
+FROM employees INNER JOIN departments
+ON employees.department = departments.department
+INNER JOIN regions
+ON employees.region_id = regions.region_id;
+
+-- return all of the department names from both the employees and departments table
+SELECT DISTINCT employees.department, departments.department
+FROM employees FULL OUTER JOIN departments
+ON employees.department = departments.department;
+
+-- return only the departments that exist in the employees table that do no exist in the departments table
+SELECT DISTINCT employees.department
+FROM employees LEFT JOIN departments
+ON employees.department = departments.department
+WHERE departments.department IS NULL;
+
+-- return all of the divisions that do not have any employees
+SELECT division 
+FROM departments LEFT JOIN employees
+ON departments.department = employees.department
+WHERE employees.department IS NULL;
