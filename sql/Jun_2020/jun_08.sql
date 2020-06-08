fd80c3fbd64cb28e06074c16e252de9a09255aa0
Clint Brodar <clintbrodar@gmail.com>
2020-06-08 13:47:51 -0700

    Over clause

--- sql/Jun_2020/jun_08.sql
+++ sql/Jun_2020/jun_08.sql
@@ -16,3 +16,27 @@ FROM products
 WHERE stock < 3 AND producent = 'CompanyA'
 ORDER BY id;
 
+-- Return the amount of employees in each department and in each region for each employee
+SELECT e.first_name, e.department, region,
+COUNT(*) OVER(PARTITION BY e.department) AS dept_employee_total,
+COUNT(*) OVER(PARTITION BY e.region_id) AS region_employee_total
+FROM employees e
+INNER JOIN regions ON e.region_id = regions.region_id;
+
+-- Return the full name, department, region, salary, average department salary, average region salary, if they make more than the average department salary and if they make more than the average salary for the region for each employee
+SELECT first_name || ' ' || last_name AS employee_name,
+department, region, salary,
+ROUND(AVG(salary) OVER(PARTITION BY department)) AS avg_dept_salary,
+ROUND(AVG(salary) OVER(PARTITION BY region)) AS avg_reg_salary,
+CASE
+	WHEN salary < ROUND(AVG(salary) OVER(PARTITION BY department))
+	THEN FALSE
+	ELSE TRUE
+END AS paid_above_dept_avg,
+CASE
+	WHEN salary < ROUND(AVG(salary) OVER(PARTITION BY region))
+	THEN FALSE
+	ELSE TRUE
+END AS paid_above_reg_avg
+FROM employees e INNER JOIN regions
+ON e.region_id = regions.region_id;
