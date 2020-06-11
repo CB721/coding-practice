8e02d964ff2764bde0576e2b9c672619b15df648
Clint Brodar <clintbrodar@gmail.com>
2020-06-10 21:22:57 -0700

    Grouping sets, rollup and cube

--- sql/Jun_2020/jun_10.sql
+++ sql/Jun_2020/jun_10.sql
@@ -13,3 +13,25 @@ SELECT first_name, last_name, department, salary,
 LAG(salary) OVER(ORDER BY salary DESC) AS closest_higher_salary,
 LEAD(salary) OVER(ORDER BY salary DESC) AS closest_lower_salary
 FROM employees;
+
+-- Return the sum of units sold by continent, country and city, the total units sold and the percentage of sales for each continent, country and city
+SELECT continent, country, city, 
+	SUM(units_sold) AS units_sold,
+	(SELECT SUM(units_sold) FROM sales) AS total_units_sold,
+	SUM(units_sold) / (SELECT SUM(units_sold) FROM sales)::decimal*100 AS units_sold_percentage 
+FROM sales
+GROUP BY GROUPING SETS(continent, country, city)
+ORDER BY continent, country, city;
+
+-- Return the sum of units sold grouped by continent, country and city, just the continent and country as well as just the continent
+SELECT continent, country, city, 
+	SUM(units_sold) AS units_sold 
+FROM sales
+GROUP BY ROLLUP(continent, country, city)
+ORDER BY continent, country, city;
+
+-- Return the sum of units sold grouped by all combinations of continent, country and city
+SELECT continent, country, city, 
+	SUM(units_sold) AS units_sold 
+FROM sales
+GROUP BY CUBE(continent, country, city);
