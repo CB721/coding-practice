9a64a931977bb7f36059f774c06885c339639e94
Clint Brodar <clintbrodar@gmail.com>
2020-06-12 10:40:45 -0700

    Timestamp conversion

--- js/Jun2020/jun12.js
+++ js/Jun2020/jun12.js
@@ -123,10 +123,42 @@ function score(dice) {
     return gameTotal;
 }
 
-console.log(score([2, 3, 4, 6, 2])); // 0
-console.log(score([4, 4, 4, 3, 3])); // 400
-console.log(score([2, 4, 4, 5, 4])); // 450
-console.log(score([1, 1, 1, 5, 1])); // 1150
-console.log(score([5, 1, 3, 4, 1])); // 250
-console.log(score([1, 1, 1, 3, 1])); // 1100
-console.log(score([2, 4, 4, 5, 4])); // 450
+// console.log(score([2, 3, 4, 6, 2])); // 0
+// console.log(score([4, 4, 4, 3, 3])); // 400
+// console.log(score([2, 4, 4, 5, 4])); // 450
+// console.log(score([1, 1, 1, 5, 1])); // 1150
+// console.log(score([5, 1, 3, 4, 1])); // 250
+// console.log(score([1, 1, 1, 3, 1])); // 1100
+// console.log(score([2, 4, 4, 5, 4])); // 450
+
+// given an array of timestamps, return the total time
+// expected output: '4 hours, 34 minutes and 0 seconds'
+function totalTime(timeStamps) {
+    // create an array of seconds
+    const seconds = timeStamps.map(time => {
+        // split into minutes and seconds
+        const [mins, secs] = time.split(":")
+            // convert minutes and seconds into numbers
+            .map(parseFloat);
+        // there are 60 seconds in each minute
+        return (mins * 60) + secs;
+    })
+        // reduce array of seconds into one number
+        .reduce((total, secs) => total + secs);
+    // convert seconds into hours, minutes and seconds
+    // keep track of how many seconds are left
+    let secondsLeft = seconds;
+    // there are are 3600 seconds in an hour
+    const hours = Math.floor(secondsLeft / 3600);
+    // update the seconds left to be the remainder after the hours are subtracted
+    secondsLeft = secondsLeft % 3600;
+    // there are 60 seconds in a minutes
+    const minutes = Math.floor(secondsLeft / 60);
+    // update the seconds left to be the remainder after the minutes are subtracted
+    secondsLeft = secondsLeft % 60;
+    return `${hours} hours, ${minutes} minutes and ${secondsLeft} seconds`;
+}
+
+console.log(totalTime(["2:15", "4:23", "7:01", "1:56", "2:32", "3:12", "5:01"])); // 0 hours, 26 minutes and 20 seconds
+console.log(totalTime(["12:25", "2:38", "1:59", "10:36", "8:44", "8:12", "24:52"])); // 1 hours, 9 minutes and 26 seconds
+console.log(totalTime(["2:15", "4:23", "7:01", "1:56", "2:32", "3:12", "5:01", "12:25", "2:38", "1:59", "10:36", "8:44", "8:12", "24:52", "23:34", "12:03"])); // 2 hours, 11 minutes and 23 seconds
