9f0f502a5608a67d76039e7c5e18a5c5c8a32ca5
Clint Brodar <clintbrodar@gmail.com>
2020-06-23 13:08:48 -0700

    Robots refactor

--- js/2020/Jun/jun23.js
+++ js/2020/Jun/jun23.js
@@ -11,47 +11,80 @@
 // If both robots run out of tactics, whoever has the most health wins. Return the message "{Name} has won the fight."
 // If both robots run out of tactics and are tied for health, the fight is a draw. Return "The fight was a draw."
 
+// function fight(robot1, robot2, tactics) {
+//     let currPlayer = robot1.speed >= robot2.speed ? 1 : 2;
+//     let winner = "";
+//     let robot1Health = robot1.health;
+//     let robot2Health = robot2.health;
+//     let length = robot1.tactics.length > 0 ? robot1.tactics.length : robot2.tactics.length;
+//     for (let i = 0; i < length; i++) {
+//         if (currPlayer === 1) {
+//             if (robot1.tactics && robot1.tactics[i]) {
+//                 robot2Health -= tactics[robot1.tactics[i]];
+//             }
+//             if (robot2Health <= 0) {
+//                 winner = robot1.name;
+//                 break;
+//             } else if(robot2.tactics && robot2.tactics[i]) {
+//                 robot1Health -= tactics[robot2.tactics[i]];
+//             }
+//             if (robot1Health <= 0) {
+//                 winner = robot2.name;
+//                 break;
+//             }
+//             currPlayer = 2;
+//         } else {
+//             if (robot2.tactics && robot2.tactics[i]) {
+//                 robot1Health -= tactics[robot2.tactics[i]];
+//             }
+//             if (robot1Health <= 0) {
+//                 winner = robot2.name;
+//                 break;
+//             } else if(robot1.tactics && robot1.tactics[i]) {
+//                 robot2Health -= tactics[robot1.tactics[i]];
+//             }
+//             if (robot2Health <= 0) {
+//                 winner = robot1.name;
+//                 break;
+//             }
+//             currPlayer = 1;
+//         }
+//     }
+//     if (robot1Health > robot2Health) winner = robot1.name;
+//     if (robot2Health > robot1Health) winner = robot2.name;
+//     return winner ? `${winner} has won the fight.` : "The fight was a draw.";
+// }
 function fight(robot1, robot2, tactics) {
-    let currPlayer = robot1.speed >= robot2.speed ? 1 : 2;
-    let winner = "";
-    let robot1Health = robot1.health;
-    let robot2Health = robot2.health;
-    let length = robot1.tactics.length > 0 ? robot1.tactics.length : robot2.tactics.length;
-    for (let i = 0; i < length; i++) {
-        if (currPlayer === 1) {
-            if (robot1.tactics && robot1.tactics[i]) {
-                robot2Health -= tactics[robot1.tactics[i]];
-            }
-            if (robot2Health <= 0) {
-                winner = robot1.name;
-                break;
-            } else if(robot2.tactics && robot2.tactics[i]) {
-                robot1Health -= tactics[robot2.tactics[i]];
-            }
-            if (robot1Health <= 0) {
-                winner = robot2.name;
-                break;
-            }
-            currPlayer = 2;
-        } else {
-            if (robot2.tactics && robot2.tactics[i]) {
-                robot1Health -= tactics[robot2.tactics[i]];
-            }
-            if (robot1Health <= 0) {
-                winner = robot2.name;
-                break;
-            } else if(robot1.tactics && robot1.tactics[i]) {
-                robot2Health -= tactics[robot1.tactics[i]];
-            }
-            if (robot2Health <= 0) {
-                winner = robot1.name;
-                break;
-            }
-            currPlayer = 1;
-        }
+    // if the first robot's speed is less than the second
+    if (robot1.speed < robot2.speed) {
+        // switch their values
+        [robot1, robot2] = [robot2, robot1];
     }
-    if (robot1Health > robot2Health) winner = robot1.name;
-    if (robot2Health > robot1Health) winner = robot2.name;
+    // keep iterating until tactics from both robots has been used or a robot has been defeated
+    while (robot1.tactics.length || robot2.tactics.length) {
+        // since the order of the robots has been switched, we can always start on the first robot
+
+        // if the first robot has any attacks left
+        if (robot1.tactics.length) {
+            // subtract from the second robots health and remove from first robots' tactics array
+            robot2.health -= tactics[robot1.tactics.shift()];
+        }
+
+        // if the second robot doesn't have any health left, exit the loop
+        if (robot2.health <= 0) break;
+
+        // if the second robot has any attacks left
+        if (robot2.tactics.length) {
+            // subtract from the first robots health and remove from second robots' tactics array
+            robot1.health -= tactics[robot2.tactics.shift()];
+        }
+
+        // if the second robot doesn't have any health left, exit the loop
+        if (robot1.health <= 0) break;
+    }
+    let winner = ""
+    if (robot1.health > robot2.health) winner = robot1.name;
+    if (robot2.health > robot1.health) winner = robot2.name;
     return winner ? `${winner} has won the fight.` : "The fight was a draw.";
 }
 
