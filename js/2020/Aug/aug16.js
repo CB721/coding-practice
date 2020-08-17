eda0ba4b823fb0644198feedb4ed0217c4d3ea33
Clint Brodar <clintbrodar@gmail.com>
2020-08-16 17:26:37 -0700

    Print odd numbers

--- js/2020/Aug/aug16.js
+++ js/2020/Aug/aug16.js
@@ -5,5 +5,21 @@ function findNumber(arr, k) {
     return arr.indexOf(k) > -1 ? "Yes" : "No";
 }
 
-console.log(findNumber([1, 34, 2, 45], 2)) // Yes
-console.log(findNumber([1, 34, 2, 45], 5)) // No
+// console.log(findNumber([1, 34, 2, 45], 2)) // Yes
+// console.log(findNumber([1, 34, 2, 45], 5)) // No
+
+// Given two numbers l and r, print all of the odd numbers between them (inclusive)
+// l is the left part of the range
+// r is the right part of the range
+// it must return an array of all odd numbers
+
+function oddNumbers(l, r) {
+    const outArr = [];
+    for (let i = l; i <= r; i++) {
+        if (i % 2 > 0) outArr.push(i);
+    }
+    return outArr;
+}
+
+console.log(oddNumbers(3, 14)) // [ 3, 5, 7, 9, 11, 13 ]
+console.log(oddNumbers(22, 77)) // [ 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77 ]
