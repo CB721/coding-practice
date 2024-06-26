d6c919db98b624b014248347075ee19a80022f87
Clint Brodar <clintbrodar@gmail.com>
2024-06-26 13:43:09 -0700

    args length

--- js/2024/Jun/jun26.js
+++ js/2024/Jun/jun26.js
@@ -40,7 +40,18 @@ function quickSelect(arr, k) {
   }
 }
 
-console.log(quickSelect([3, 1, 4, 4, 2], 2)); // 2
-console.log(quickSelect([3, 1, 4, 4, 2], 3)); // 3
-console.log(quickSelect([4, 5, 1], 2)); // 4
-console.log(quickSelect([120, 457834, 23, 42346, 77674, 834, 2341, 435, 3, 45378, 878, 234578, 45743, 848, 44, -1, 47, 0, 74], 13)); // 2341
+// console.log(quickSelect([3, 1, 4, 4, 2], 2)); // 2
+// console.log(quickSelect([3, 1, 4, 4, 2], 3)); // 3
+// console.log(quickSelect([4, 5, 1], 2)); // 4
+// console.log(quickSelect([120, 457834, 23, 42346, 77674, 834, 2341, 435, 3, 45378, 878, 234578, 45743, 848, 44, -1, 47, 0, 74], 13)); // 2341
+
+// Return Length of Arguments Passed
+// Write a function argumentsLength that returns the count of arguments passed to it.
+
+var argumentsLength = function (...args) {
+  return args.length;
+};
+
+// console.log(argumentsLength(1, 2, 3)); // 3
+// console.log(argumentsLength(1, 2, null, 10, 5)); // 5
+// console.log(argumentsLength('foo', { bar: 'rab' }, 'baz', 'bin')); // 4
