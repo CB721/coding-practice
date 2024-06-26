408c7b6a4330dcbceec004fd03e270301fca7426
Clint Brodar <clintbrodar@gmail.com>
2024-06-26 14:45:35 -0700

    subsets

--- js/2024/Jun/jun26.js
+++ js/2024/Jun/jun26.js
@@ -61,22 +61,22 @@ var argumentsLength = function (...args) {
 
 // Return the score of s.
 // s will have at least two characters.
-var scoreOfString = function(s) {
-    // let total = Math.abs(s.charCodeAt(0) - s.charCodeAt(1));
+var scoreOfString = function (s) {
+  // let total = Math.abs(s.charCodeAt(0) - s.charCodeAt(1));
 
-    // for (let i = 1; i < s.length - 1; i++) {
-    //   total += Math.abs(s.charCodeAt(i) - s.charCodeAt(i + 1));
-    // }
+  // for (let i = 1; i < s.length - 1; i++) {
+  //   total += Math.abs(s.charCodeAt(i) - s.charCodeAt(i + 1));
+  // }
 
-    // return total;
+  // return total;
 
-    return [...s].reduce((acc, curr, i, arr) => {
-      if (i < arr.length - 1) {
-        acc += Math.abs(curr.charCodeAt(0) - arr[i + 1].charCodeAt(0));
-      
-      }
-      return acc;
-    }, 0);
+  return [...s].reduce((acc, curr, i, arr) => {
+    if (i < arr.length - 1) {
+      acc += Math.abs(curr.charCodeAt(0) - arr[i + 1].charCodeAt(0));
+
+    }
+    return acc;
+  }, 0);
 };
 
 // console.log(scoreOfString('abc')); // 2
@@ -86,7 +86,7 @@ var scoreOfString = function(s) {
 // Add Two Promises
 // Given two promises promise1 and promise2, return a new promise. promise1 and promise2 will both resolve with a number. The returned promise should resolve with the sum of the two numbers.
 
-var addTwoPromises = async function(...args) {
+var addTwoPromises = async function (...args) {
   const [val1, val2] = await Promise.all(args);
 
   return val1 + val2;
@@ -95,3 +95,47 @@ var addTwoPromises = async function(...args) {
 // addTwoPromises(Promise.resolve(3), Promise.resolve(4)).then((res) => {
 //   console.log(res); // 7
 // });
+
+// Subsets
+// Given an integer array nums of unique elements, return all possible subsets.
+
+// The solution set must not contain duplicate subsets. Return the solution in any order.
+// All the numbers of nums are unique.
+
+var subsets = function (nums) {
+  // const output = [[]];
+
+  // for (let i = 0; i < nums.length; i++) {
+  //   console.log('------ outer loop ------')
+  //   console.log('nums[i]', nums[i])
+  //   const length = output.length;
+  //   console.log('length', length);
+  //   for (let j = 0; j < length; j++) {
+  //     console.log('------ inner loop ------')
+  //     console.log('output[j]', output[j]);
+  //     output.push([...output[j], nums[i]]);
+  //     console.log('output', output);
+  //   }
+  // }
+
+  // return output;
+
+  const output = [];
+  const temp = [];
+
+  const getSubset = (start = 0) => {
+    output.push([...temp]);
+
+    for (let i = start; i < nums.length; i++) {
+      temp.push(nums[i]);
+      getSubset(i + 1);
+      temp.pop();
+    }
+  }
+
+  getSubset();
+
+  return output;
+};
+
+console.log(subsets([1, 2, 3])); // [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
