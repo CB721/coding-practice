5ab192a4b1e5748ef22176b3ab0ae1dfbc52f2e4
Clint Brodar <clintbrodar@gmail.com>
2024-06-26 15:16:09 -0700

    remove duplicates

--- js/2024/Jun/jun26.js
+++ js/2024/Jun/jun26.js
@@ -138,4 +138,36 @@ var subsets = function (nums) {
   return output;
 };
 
-console.log(subsets([1, 2, 3])); // [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+// console.log(subsets([1, 2, 3])); // [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+// Remove duplicates from a sorted array
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+// Example 1:
+// Input: nums = [1,1,2]
+// Output: 2, nums = [1,2,_]
+
+var removeDuplicates = function(nums) {
+  let uniqueCount = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    console.log('------------------')
+    console.log('nums[i]', nums[i]);
+    console.log('nums[uniqueCount - 1]', nums[uniqueCount - 1]);
+    if (nums[i] !== nums[uniqueCount - 1]) {
+      nums[uniqueCount] = nums[i];
+      uniqueCount++;
+    }
+    console.log('nums', nums);
+    console.log('uniqueCount', uniqueCount);
+  }
+  console.log('final nums', nums);
+  return uniqueCount;
+};
+
+// console.log(removeDuplicates([1, 1, 2])); // 2
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4])); // 5
