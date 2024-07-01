9ee205b2a407dc0420cc9c614fd8ff0cb799da3d
Clint Brodar <clintbrodar@gmail.com>
2024-07-01 16:35:17 -0700

    trapping rain water

--- js/2024/jul/jul1.js
+++ js/2024/jul/jul1.js
@@ -58,7 +58,7 @@ function reverse(str) {
  * @param {number} n
  * @return {ListNode}
  */
-var removeNthFromEnd = function(head, n) {
+var removeNthFromEnd = function (head, n) {
   // O(n) complexity solution because, at most, the method iterates over every node in the linked list only once.
   let tempNode = new Node(0);
   tempNode.next = head;
@@ -110,7 +110,7 @@ var removeNthFromEnd = function(head, n) {
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
+var maxArea = function (height) {
   // O(n) complexity solution because, at most, the method iterates over every element in the array only once (with two pointers).
   let maxArea = 0;
   let left = 0;
@@ -130,4 +130,56 @@ var maxArea = function(height) {
   return maxArea;
 };
 
-console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])) // 49
+// console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])) // 49
+
+// trapping rain water
+// Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function (height) {
+  // Initialize two pointers, 'left' and 'right', at the start and end of the array respectively
+  let left = 0;
+  let right = height.length - 1;
+
+  // Initialize two variables, 'leftMax' and 'rightMax', to keep track of the maximum height seen so far from the left and right respectively
+  let leftMax = 0;
+  let rightMax = 0;
+
+  // Initialize a variable 'water' to keep track of the total amount of water that can be trapped
+  let water = 0;
+
+  // Continue the loop until the 'left' pointer is less than the 'right' pointer
+  while (left < right) {
+    // If the height at the 'left' pointer is less than the height at the 'right' pointer
+    if (height[left] < height[right]) {
+      // If the height at the 'left' pointer is greater than or equal to 'leftMax', update 'leftMax'
+      if (height[left] >= leftMax) {
+        leftMax = height[left];
+      } else {
+        // Otherwise, add the difference between 'leftMax' and the height at the 'left' pointer to 'water'
+        water += leftMax - height[left];
+      }
+      // Move the 'left' pointer one step to the right
+      left++;
+    } else {
+      // If the height at the 'right' pointer is greater than or equal to 'rightMax', update 'rightMax'
+      if (height[right] >= rightMax) {
+        rightMax = height[right];
+      } else {
+        // Otherwise, add the difference between 'rightMax' and the height at the 'right' pointer to 'water'
+        water += rightMax - height[right];
+      }
+      // Move the 'right' pointer one step to the left
+      right--;
+    }
+  }
+
+  // Return the total amount of water that can be trapped
+  return water;
+};
+
+console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])) // 6
+// console.log(trap([4,2,0,3,2,5])) // 9
