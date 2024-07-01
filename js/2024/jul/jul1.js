70b4933c556e0a3a167b63ff9a88faaebf94c6c2
Clint Brodar <clintbrodar@gmail.com>
2024-07-01 15:16:26 -0700

    remove nth node from end of linked list

--- js/2024/jul/jul1.js
+++ js/2024/jul/jul1.js
@@ -1,3 +1,5 @@
+const { Node } = require('../../DataStructures/linkedList')
+
 // length of last word
 // Given a string s consisting of words and spaces, return the length of the last word in the string.
 
@@ -44,3 +46,54 @@ function reverse(str) {
 
   return output;
 }
+
+// console.log(reverse('abc')) // cba
+
+// remove nth node from end of list
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+  // O(n) complexity solution because, at most, the method iterates over every node in the linked list only once.
+  let tempNode = new Node(0);
+  tempNode.next = head;
+  let first = tempNode;
+  let second = tempNode;
+
+  for (let i = 1; i <= n + 1; i++) {
+    first = first.next;
+  }
+
+  while (first !== null) {
+    first = first.next;
+    second = second.next;
+  }
+
+  second.next = second.next.next;
+
+  return tempNode.next;
+};
+
+const a = new Node(1);
+const b = new Node(2);
+const c = new Node(3);
+const d = new Node(4);
+const e = new Node(5);
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+console.log(removeNthFromEnd(a, 2)) // [1, 2, 3, 5]
+
+const f = new Node(1);
+console.log(removeNthFromEnd(f, 1)) // []
+
+const g = new Node(1);
+const h = new Node(2);
+g.next = h;
+console.log(removeNthFromEnd(g, 1)) // [1]
